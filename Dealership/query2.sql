ebd8a30e87484e0345920c59df91b0f1362a7c17
Nik0la074 <151242185+Nik0la074@users.noreply.github.com>
2025-01-25 10:24:49 +0200

    Add files via upload

--- Dealership/query2.sql
+++ Dealership/query2.sql
@@ -1,4 +1,4 @@
--- брой на транзакциите за всеки автомобил
+-- 2. Брой на транзакциите за всеки автомобил
 SELECT car.Make, car.Model, COUNT(ct.TransactionID) AS TotalTransactions
 FROM Cars car
 LEFT JOIN CarTransactions ct ON car.CarID = ct.CarID
